0adc1e0011fe83b021dd3cd35178ecaebffa721e
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2020-11-06 10:09:04 +0800

    啥都没干

--- pages/studentManage/studentManage.js
+++ pages/studentManage/studentManage.js
@@ -5,16 +5,96 @@ Page({
    * 页面的初始数据
    */
   data: {
+    isAddShow: false,
+    isNameError: false,
+    isIdError: false,
+    studentList: [],
+    newStudentName: '',
+    newStudentId: '',
+  },
 
+  toAddStudent: function () {
+    this.setData({
+      isAddShow: true
+    })
+  },
+
+  onSaveStudent: function () {
+    //数据正确性检查
+    if (this.data.newStudentName == '') {
+      this.setData({
+        isNameError: true
+      })
+      return
+    }
+    if (this.data.newStudentId == '') {
+      this.setData({
+        isIdError: true
+      })
+      return
+    }
+    //提交数据
+    var newStudent = {
+      "studentName": this.data.newStudentName,
+      "studentId":this.data.newStudentId
+    }
+    this.data.studentList.push(newStudent) //替换
+    this.setData({
+      isNameError: false,
+      isIdError:false,
+      isAddShow: false,
+      studentList: this.data.studentList,
+      newStudentName: '',
+      newStudentId:''
+    })
+  },
+  onNameChange(event) {
+    if (event.detail == '') {
+      this.setData({
+        isNameError: true
+      })
+    } else {
+      this.setData({
+        isNameError: false
+      })
+    }
+    this.setData({
+      newStudentName: event.detail
+    })
+  },
+
+  onIdChange(event) {
+    if (event.detail == '') {
+      this.setData({
+        isIdError: true
+      })
+    } else {
+      this.setData({
+        isIdError: false
+      })
+    }
+    this.setData({
+      newStudentId: event.detail
+    })
+  },
+
+  onClickHide(event) {
+    this.setData({
+      isNameError: false,
+      isIdError: false,
+      isAddShow: false
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.loadStudentList()
   },
-
+  loadStudentList: function () {
+    //请求后端的数据
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/teacherManage/teacherManage.js
+++ pages/teacherManage/teacherManage.js
@@ -5,20 +5,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isAddShow: false,
+    isError: false,
+    newTeacherName: '',
+    teacherList: [{
+      "teacherName": "testname",
+      "teacherId": "123124",
+    }]
   },
   toAddTeacher: function () {
-    wx.navigateTo({
-      url: '../addTeacher/addTeacher',
+    this.setData({
+      isAddShow: true
+    })
+  },
+  
+  onSaveTeacher: function () {
+    //数据正确性检查
+    if (this.data.newTeacherName == '') {
+      this.setData({
+        isError: true
+      })
+      return
+    }
+    //提交数据
+    var newTeacher = {
+      "teacherName": this.data.newTeacherName
+    }
+    this.data.teacherList.push(newTeacher)//替换
+    this.setData({
+      isError:false,
+      isAddShow: false,
+      teacherList: this.data.teacherList,
+      newTeacherName:''
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //加载教师列表
+    this.loadTeacherList()
 
   },
 
+  loadTeacherList: function () {
+    //请求后端的数据
+  },
+  onNameChange(event) {
+    if(event.detail == ''){
+      this.setData({
+        isError:true
+      })
+    }else{
+      this.setData({
+        isError:false
+      })
+    }
+    this.setData({
+      newTeacherName: event.detail
+    })
+  },
+  onClickHide(event) {
+    this.setData({
+      isError:false,
+      isAddShow: false
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
