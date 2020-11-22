319c3fd7b67a9bce892a6017b82c35df5b8877ea
Kudryavka <50348668+kudryavka1013@users.noreply.github.com>
2020-11-22 15:46:26 +0800

    用户登录

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 //app.js
 App({
   onLaunch: function () {
-    
+    var that = this
+    //获取本地登录信息
+    wx.getStorage({
+      key: 'loginInfo',
+      //已经登录
+      success(res) {
+        let loginInfo = res.data
+        that.globalData.userInfo = res.data
+        switch (loginInfo.user.type) {
+          case admin:
+            wx.redirectTo({
+              url:'/pages/admin/index'
+            })
+          case teacher:
+            wx.redirectTo({
+              url:'/pages/teacher/index'
+            })
+        }
+      },
+      fail() {
+
+      }
+    })
   },
   globalData: {
     userInfo: null,
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-
+    isLogin: false,
+    userInfo: null,
+    isAuthenticated: false
   },
 
   toAdmin: function () {
@@ -16,5 +18,62 @@ Page({
     wx.redirectTo({
       url: '../teacher/teacher',
     })
-  }
+  },
+
+  login: function () {
+    var that = this
+    // 调用接口获取登录凭证
+    wx.login({
+      success(loginRes) {
+        console.log(loginRes)
+        if (loginRes.code) {
+          //发送code到后端验证身份
+          wx.request({
+            url: 'https://test.com/onLogin',
+            method: 'POST',
+            data: {
+              code: loginRes.code
+            },
+            success: (res) => {
+              console.log(res)
+              that.setData({
+                userInfo: res.data
+              })
+              let userInfo = res.data
+              // 已经在系统中认证过
+              if (userInfo.isAuthenticated) {
+                // 保存登陆状态和信息到本地
+                try {
+                  wx.setStorageSync('loginInfo', userInfo)
+                } catch (e) {
+                  console.log(e)
+                }
+                app.globalData.userInfo = userInfo
+                // 提示重新打开小程序
+                that.setData({
+                  isAuthenticated: true
+                })
+              } else {
+                // 跳转到认证页
+                that.setData({
+                  isLogin: true
+                })
+              }
+            }
+          })
+        } else {
+          console.log('登录失败！' + loginRes.errMsg)
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    if (app.globalData.userInfo == null) {
+      this.login()
+    }
+  },
 })
